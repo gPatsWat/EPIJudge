5a8ecf0f6eb17a976ae389b46e7c4081ca981b07
gspathak <gspathak@uwaterloo.ca>
2022-10-30 21:48:34 -0400

    reverse_bits

--- epi_judge_cpp/reverse_bits.cc
+++ epi_judge_cpp/reverse_bits.cc
@@ -1,7 +1,12 @@
 #include "test_framework/generic_test.h"
 unsigned long long ReverseBits(unsigned long long x) {
-  // TODO - you fill in here.
-  return 0;
+  unsigned long long rev = 0;
+  for(int i = 0;i<64;i++) {
+    rev <<= 1;
+    rev |= x & 0x0000000000000001;
+    x >>= 1;
+  }
+  return rev;
 }
 
 int main(int argc, char* argv[]) {
